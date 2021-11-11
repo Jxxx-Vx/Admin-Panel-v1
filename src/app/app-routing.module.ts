6ee92a72393772ecee32f96f6363ef3fb7096f11
Jxxx-Vx <john.ctvo@hotmail.com>
2021-11-11 15:01:34 -0800

    Testing login page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  {path: '', pathMatch:'full', redirectTo: '/login'},
+  {path: 'login',component:LoginComponent},
+  {path: 'welcome',component:WelcomeComponent}
+  // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  //{ path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({
