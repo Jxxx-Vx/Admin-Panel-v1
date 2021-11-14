e2ee2e71db7a0268b25d13196334a02492dcf22d
Matthew Hoang <hoangmatthew1999@gmail.com>
2021-11-13 16:34:47 -0800

    started to work on the navigation part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {path: '', pathMatch:'full', redirectTo: '/login'},
   {path: 'login',component:LoginComponent},
   {path: 'welcome',component:WelcomeComponent}
+
   // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   //{ path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
