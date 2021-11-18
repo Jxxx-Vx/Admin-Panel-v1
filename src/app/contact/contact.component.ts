845b3b114cc84ffe7c452ae8a9024b1a2dfd3a28
Matthew Hoang <hoangmatthew1999@gmail.com>
2021-11-17 19:00:50 -0800

    added tabs to contacts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,22 +4,19 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'nz-demo-list-simple',
-  template: `<h3 [ngStyle]="{ 'margin-bottom.px': 16 }">Default Size</h3>
-  <nz-list nzBordered nzHeader="Header" nzFooter="Footer">
-    <nz-list-item *ngFor="let item of data">
-      <span nz-typography><mark>[ITEM]</mark></span>
-      {{ item }}
-    </nz-list-item>
-  </nz-list>
-
-  <h3 [ngStyle]="{ margin: '16px 0' }">Small Size</h3>
-  <nz-list nzBordered nzSize="small">
-    <nz-list-header>Header</nz-list-header>
-    <nz-list-item *ngFor="let item of data">item</nz-list-item>
-    <nz-list-footer>Footer</nz-list-footer>
-  </nz-list>
-
+  template: `
+  
   <h3 [ngStyle]="{ margin: '16px 0' }">Large Size</h3>
+  <nz-tabset>
+      <nz-tab nzTitle="All ">Content of Tab Pane 1</nz-tab>
+      <nz-tab nzTitle="Manager">Content of Tab Pane 2</nz-tab>
+      <nz-tab nzTitle="Coaches">Content of Tab Pane 3</nz-tab>
+      <nz-tab nzTitle="Advisors">Content of Tab Pane 4</nz-tab>
+      <nz-tab nzTitle="Specialist">Content of Tab Pane 5</nz-tab>
+      <nz-tab nzTitle="User">Content of Tab Pane 6</nz-tab>
+
+    </nz-tabset>
+
   <ul nz-list [nzDataSource]="data" nzBordered nzSize="large">
     <nz-list-header>Header</nz-list-header>
     <li nz-list-item *ngFor="let item of data" nzNoFlex>
@@ -31,17 +28,18 @@ import { NzMessageService } from 'ng-zorro-antd/message';
       {{ item }}
     </li>
     <nz-list-footer>Footer</nz-list-footer>
-  </ul>`,
+  </ul>
+  `,
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
 
   data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.'
+    'Ryan',
+    'Dave Marshall',
+    'John Smith',
+    'Michael Rodriguez',
+    'Kevin Nguyen '
   ];
   constructor(public msg: NzMessageService) { }
 
